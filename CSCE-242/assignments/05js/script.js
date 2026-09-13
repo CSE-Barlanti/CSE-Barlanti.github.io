e248c2f76b6b96f856a05d81b1eef8239fc33830
Dean Barlanti <CSE-Barlanti>
2026-09-13 14:54:59 -0400

    Using java script to make my website interactive

---
+++ CSCE-242/assignments/05js/script.js
@@ -0,0 +1,28 @@
+const speechColumn = document.querySelector("#speech-column");
+const speechBubble = document.querySelector("#speech-bubble");
+const beverage = document.querySelector("#beverage");
+const beverageMessage = document.querySelector("#beverage-message");
+const sunImage = document.querySelector("#sun-image");
+const stickerColumn = document.querySelector("#sticker-column");
+
+const showSpeech = () => {
+    speechBubble.classList.remove("hidden");
+};
+
+const showBeverageMessage = () => {
+    beverageMessage.innerHTML = '${beverage.value}: Nice Choice!';
+};
+
+const addSticker = () => {
+    const sticker = document.createElement("div");
+
+    sticker.classList.add("sticker");
+    sticker.innerHTML = "☀️";
+
+    stickerColumn.append(sticker);
+}
+
+speechColumn.addEventListener("click", showSpeech);
+beverage.addEventListener("change", showBeverageMessage);
+sunImage.addEventListener("click", addSticker)
+
