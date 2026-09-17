c00d48938334c75b530d7c779191d35fc50d0dec
Dean Barlanti <CSE-Barlanti>
2026-09-17 11:48:56 -0400

    Intro

--- CSCE-242/excercises/04jss/scripts/intro.js
+++ CSCE-242/excercises/04jss/scripts/intro.js
@@ -15,14 +15,74 @@ document.getElementById("btn-bounce").onclick = (e) => {
     document.getElementById("ball").classList.toggle("bouncing-ball");
 }
 
-//When you change the number of days since you watered your plant
+//when you change the number of days since you watered your plant
 //show a message and change the image
-document.getElementByID("txt-num-days").onkeyup = (e) => {
+document.getElementById("txt-num-days").onkeyup = (e) => {
     const numDays = parseInt(e.target.value);
-    const pMessage= document.getElementById("p-plant-msg");
-    pMessage.innerHTMl = numDays;
-
+    const pMessage = document.getElementById("p-plant-msg");
+    const plantImage = document.getElementById("img-plant");
+    plantImage.classList.remove("hidden");
+    
     if(numDays <= 2){
-        pMessage.innerHTML = "Let your plant rest"
+        pMessage.innerHTML = `Let your plant rest it's only been ${numDays} day(s).`;
+        plantImage.src="https://dummyimage.com/150x150/556B2F/fff&text=Happy+Plant";
+    } else if(numDays <= 5){
+        pMessage.innerHTML = `Time to water it's been ${numDays} days.`;
+        plantImage.src="https://dummyimage.com/150x150/BDB76B/fff&text=Thirsty";
+    } else if(numDays <= 7) {
+        pMessage.innerHTML = `Oh no your plant is wilting it's been ${numDays} days.`;
+    } else {
+        pMessage.innerHTML = "Your plant is a gooner";
     }
 }
+
+//counting
+let countInterval;
+let count = 0;
+const pCount = document.getElementById("p-count");
+const btnStart = document.getElementById("btn-start");
+const btnPause = document.getElementById("btn-pause");
+const btnStop = document.getElementById("btn-stop");
+btnPause.disabled = true;
+btnStop.disabled = true;
+
+btnStart.onclick = () =>
+{
+    countInterval = setInterval(()=>{
+        pCount.innerHTML = ++count;
+    },500);
+    btnStart.disabled = true;
+    btnPause.disabled = false;
+    btnStop.disabled = false;
+};
+
+btnPause.onclick = () =>
+{
+    clearInterval(countInterval);
+    btnStart.disabled = false;
+    btnPause.disabled = true;
+    btnStop.disabled = true;
+};
+
+btnStop.onclick = () =>
+{
+    count=0;
+    pCount.innerHTML = "";
+    clearInterval(countInterval);
+    btnStart.disabled = false;
+    btnPause.disabled = true;
+    btnStop.disabled = true;
+};
+
+//date display
+setInterval(()=>{
+    const pDisplay = document.getElementById("date-display");
+    const today = new Date();
+    const month = today.getMonth();
+    const day = today.getDay();
+    const year = today.getFullYear();
+    const seconds = today.getSeconds();
+    const minutes = today.getMinutes();
+    const hours = today.getHours();
+    pDisplay.innerHTML = seconds;
+}, 1000);
