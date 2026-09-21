a9e434b3af05d8ebb7d28a85440cd1f4c86e5209
Dean Barlanti <CSE-Barlanti>
2026-09-20 21:14:39 -0400

    Java Script Exercise 1

---
+++ CSCE-242/assignments/08ifstatements/scripts/script.js
@@ -0,0 +1,51 @@
+console.log("JavaScript is Connected");
+const linkExerciseOne = document.querySelector("#link-exercise-one");
+const linkExerciseTwo = document.querySelector("#link-exercise-two");
+
+const exerciseOne = document.querySelector("#exercise-one");
+const exerciseTwo = document.querySelector("#exercise-two");
+
+linkExerciseOne.addEventListener("click", (event) => {
+    event.preventDefault();
+
+
+    exerciseOne.classList.remove("hidden");
+    exerciseTwo.classList.add("hidden");
+});
+
+linkExerciseTwo.addEventListener("click", (event) =>{
+    event.preventDefault();
+
+    exerciseOne.classList.add("hidden");
+    exerciseTwo.classList.remove("hidden");
+});
+
+const daysInput = document.querySelector("#txt-days");
+const deductionResult = document.querySelector("#deduction-result");
+const attendenceResult = document.querySelector("#attendence-result");
+
+daysInput.addEventListener("input", () =>{
+    const missedDays = Number(daysInput.value);
+
+    if(daysInput.value === ""){
+        deductionResult.textContent = "";
+        return;
+    }
+
+    const deduction = (missedDays / 25) * 7;
+
+    deductionResult.textContent = "You will lose " + deduction.toFixed(1) + "% for skipping " + missedDays + " days.";
+
+    if(missedDays = 0){
+        attendenceResult.textContent = "Perfect attendance so far - keep it up!"
+    }
+    else if(missedDays <= 3){
+        attendenceResult.textContent = "A few missed classes can add up, so stay on track."
+    }
+    else if(missedDays <= 7){
+        attendenceResult.textContent = "You are missing a noticeable part of the course."
+    }
+    else{
+        attendenceResult.textContent = "This is not an online class; you are missing valuable learning opportunities."
+    }
+})
